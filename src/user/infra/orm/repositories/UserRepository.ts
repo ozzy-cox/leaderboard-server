1a77e127afdc1d434335ef7dc0283972848d39f9
ozank <ozankoksal@hotmail.com>
2023-05-19 18:04:43 +0300

    Updated user repository to include getting top users

--- src/user/infra/orm/repositories/UserRepository.ts
+++ src/user/infra/orm/repositories/UserRepository.ts
@@ -1,8 +1,7 @@
 import { IUser } from '@/user/entities/IUser'
 import { IUserRepository } from '@/user/repositories/IUserRepository'
 import { User } from '../models/User'
-import { Connection, EntityManager, IDatabaseDriver } from '@mikro-orm/core'
-import { SqlEntityRepository } from '@mikro-orm/sqlite'
+import { SqlEntityRepository, EntityManager } from '@mikro-orm/sqlite'
 
 export class UserRepository implements IUserRepository {
   private repository: SqlEntityRepository<User>
@@ -21,5 +20,15 @@ export class UserRepository implements IUserRepository {
     this.em.persist(user)
     this.em.flush()
     return user
+  }
+
+  async getTopUsers(amount: number): Promise<IUser[]> {
+    return await this.em
+      .qb(User, 'u')
+      .select(['u.username', 'u.country', 'u.id', 'u.createdAt', 'sum(g.money_gained) as money'])
+      .join('u.games', 'g')
+      .groupBy('u.id')
+      .limit(amount)
+      .getResultList()
   }
 }
--- src/user/repositories/IUserRepository.ts
+++ src/user/repositories/IUserRepository.ts
@@ -3,4 +3,5 @@ import { IUser } from '../entities/IUser'
 export interface IUserRepository {
   createUser(username: string, country: string): Promise<IUser | undefined> // For testing purposes
   getUser(id: string): Promise<IUser | null>
+  getTopUsers(amount: number): Promise<IUser[]>
 }
