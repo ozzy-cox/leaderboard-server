28baf5589e3b978d2420b6369fa0ee09ceb367d5
ozank <ozankoksal@hotmail.com>
2023-05-21 13:23:22 +0300

    Added validation queries to compare leaderbaord to the persisted user data

--- src/user/repositories/IUserRepository.ts
+++ src/user/repositories/IUserRepository.ts
@@ -1,7 +1,8 @@
-import { IUser } from '../entities/IUser'
+import { IUser, IUserWithMoney } from '../entities/IUser'
 
 export interface IUserRepository {
-  createUser(username: string, country: string): Promise<IUser | undefined> // For testing purposes
-  getUser(id: string): Promise<IUser | null>
-  getTopUsers(amount: number): Promise<IUser[]>
+  addUser(username: string, country: string): Promise<IUser | undefined> // For testing purposes
+  getUsersById(ids: IUser['id'][]): Promise<(IUser | null)[]>
+  getTopUsers(amount: number): Promise<IUserWithMoney[]>
+  getUserRangeWithMoney(offset: number, limit: number): Promise<IUserWithMoney[]>
 }
