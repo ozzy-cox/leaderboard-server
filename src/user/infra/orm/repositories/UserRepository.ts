ab1b014a6c5534daf04d410722c5aeec9c080bf4
ozank <ozankoksal@hotmail.com>
2023-05-21 13:23:17 +0300

    Added validation queries to compare leaderbaord to the persisted user data

--- src/user/infra/orm/repositories/UserRepository.ts
+++ src/user/infra/orm/repositories/UserRepository.ts
@@ -2,6 +2,7 @@ import { IUser } from '@/user/entities/IUser'
 import { IUserRepository } from '@/user/repositories/IUserRepository'
 import { User } from '../models/User'
 import { SqlEntityRepository, EntityManager } from '@mikro-orm/sqlite'
+import { QueryOrder } from '@mikro-orm/core'
 
 export class UserRepository implements IUserRepository {
   private repository: SqlEntityRepository<User>
@@ -11,24 +12,38 @@ export class UserRepository implements IUserRepository {
     this.em = em
   }
 
-  async getUser(id: string) {
-    return await this.repository.findOne({ id: id })
+  async getUsersById(ids: IUser['id'][]) {
+    return await this.repository.find({ id: { $in: ids } }, { cache: true })
   }
 
-  async createUser(username: IUser['username'], country: IUser['country']) {
+  async addUser(username: IUser['username'], country: IUser['country']) {
     const user = new User(username, country)
     this.em.persist(user)
     this.em.flush()
     return user
   }
 
-  async getTopUsers(amount: number): Promise<IUser[]> {
+  async getTopUsers(amount: number) {
     return await this.em
       .qb(User, 'u')
       .select(['u.username', 'u.country', 'u.id', 'u.createdAt', 'sum(g.money_gained) as money'])
-      .join('u.games', 'g')
+      .leftJoin('u.games', 'g')
       .groupBy('u.id')
+      .orderBy({ money: QueryOrder.desc, id: QueryOrder.desc })
       .limit(amount)
+      .cache()
+      .getResultList()
+  }
+
+  async getUserRangeWithMoney(offset: number, limit: number) {
+    return await this.em
+      .qb(User, 'u')
+      .select(['u.username', 'u.country', 'u.id', 'u.createdAt', 'sum(g.money_gained) as money'])
+      .leftJoin('u.games', 'g')
+      .groupBy('u.id')
+      .limit(limit)
+      .offset(offset)
+      .cache()
       .getResultList()
   }
 }
