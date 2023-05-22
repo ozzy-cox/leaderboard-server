4df0185fbbd4142b0b142eca4868778d21a1088a
ozank <ozankoksal@hotmail.com>
2023-05-22 21:53:34 +0300

    Update index.js to initialize dummy data for testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { app } from './app'
+import { createDummyUsers } from './initializeDbForTests'
+import { mockContext } from './mockContext'
+import { wipeDb } from './orm'
 
 const port = 8000
+
+// Initializing mock data
+await wipeDb()
+await createDummyUsers()
+const context = await mockContext()
+await createDummyUsers()
+
+const usersWithMoney = await context.userService.getAllUsersWithMoney()
+await context.leaderboardService.updateRecords(
+  ...usersWithMoney.map((user) => ({ id: user.id, money: user.money || 0 }))
+)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
