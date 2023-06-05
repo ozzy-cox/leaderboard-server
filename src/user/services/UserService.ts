6f74b9db8b883bedc3a0eb5c47bee2118d0cd5e3
ozank <ozankoksal@hotmail.com>
2023-06-05 19:51:18 +0300

    User creation updates the username index

--- src/user/services/UserService.ts
+++ src/user/services/UserService.ts
@@ -1,9 +1,10 @@
 import { TOP_PLAYERS_COUNT } from '@/leaderboard/configs/BoardConfig'
 import { IUserRepository } from '../repositories/IUserRepository'
 import { IUser } from '../entities/IUser'
+import { IUserSearchRepository } from '../repositories/IUserSearchRepository'
 
 export class UserService {
-  constructor(private repository: IUserRepository) {}
+  constructor(private repository: IUserRepository, private cache?: IUserSearchRepository) {}
 
   async getAllUsersWithMoney() {
     // TODO should return an iterator later
@@ -20,10 +21,20 @@ export class UserService {
   }
 
   async createUser(username: IUser['username'], country: IUser['country']) {
-    return await this.repository.addUser(username, country)
+    const newUser = await this.repository.addUser(username, country)
+    if (this.cache && newUser) {
+      await this.cache?.cacheUsername(newUser)
+    }
+    return newUser
   }
 
   async getUser(id: IUser['id']) {
     return (await this.repository.getUsersById([id]))[0]
+  }
+
+  async updateUserCache(user: IUser) {
+    if (this.cache) {
+      await this.cache?.cacheUsername(user)
+    }
   }
 }
