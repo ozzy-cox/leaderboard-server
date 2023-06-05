c25d107eebf8a25ddd2174da64a90f649a6be5f2
ozank <ozankoksal@hotmail.com>
2023-06-05 19:51:56 +0300

    Added user search repo to mockcontext

--- src/mockContext.ts
+++ src/mockContext.ts
@@ -7,17 +7,21 @@ import { LeaderboardRepository } from './leaderboard/infra/redis/repositories/Le
 import { LeaderboardService } from './leaderboard/services/LeaderboardService'
 import { GameService } from './game/services/GameService'
 import { GameRepository } from './game/infra/orm/repositories/GameRepository'
+import { UserSearchRepository } from './user/infra/orm/repositories/UserSearchRepository'
+import { REDIS } from './redis'
 
 export const mockContext = async (): Promise<Context> => {
   // TODO change orm hook to get different versions of dev and prod
   const orm = await ORM.getInstance()
   const redis = new redisMock()
+  const client = await REDIS.getInstance()
   const em = orm.em.fork()
   const userRepository = new UserRepository(em)
   const leaderboardRepository = new LeaderboardRepository(redis)
   const gameRepository = new GameRepository(em)
+  const userSearchRepository = new UserSearchRepository(client)
   return {
-    userService: new UserService(userRepository),
+    userService: new UserService(userRepository, userSearchRepository),
     leaderboardService: new LeaderboardService(leaderboardRepository),
     gameService: new GameService(gameRepository)
   }
